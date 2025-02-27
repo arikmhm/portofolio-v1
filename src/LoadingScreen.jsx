f91670d50d6c5088a7495498048e1ecd561066b4
arikmhm <mhm.ariyanto@gmail.com>
2025-02-27 09:19:39 +0700

    test update

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -14,7 +14,7 @@ const LoadingScreen = ({ onComplete }) => {
   }, [onComplete]);
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-xl font-light">Loading</div>
+      <div className="mb-4 text-xl font-light">Loading........</div>
 
       <div className="w-[200px] h-[2px] bg-gray-900 relative overflow-hidden">
         <div className="w-[40%] h-full bg-white shadow[0_0_15px_#3b82f6] animate-loading-bar"></div>
